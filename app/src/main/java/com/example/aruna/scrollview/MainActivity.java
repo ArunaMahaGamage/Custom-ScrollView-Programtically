bb58bde212b22d2229d3b67dc46ae1af1aa131ba
Aruna <admgbit@gmail.com>
2019-02-08 13:17:12 +0530

    first commit

---
+++ app/src/main/java/com/example/aruna/scrollview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.aruna.scrollview;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = getBaseContext();
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.llscrol);
+
+        for (int i = 0; i < 20; i++) {
+
+            View v = LayoutInflater.from(context).inflate(R.layout.custom_lay, ll, false);
+
+            v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            ll.addView(v);
+
+            ll.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+        }
+
+    }
+}
